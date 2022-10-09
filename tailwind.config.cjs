879ae9b819226906e8c69a955fa4fa42266070b6
Finn <34718806+FinnDore@users.noreply.github.com>
2022-10-09 16:22:19 +0100

    added some timezone stuff

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+            },
+        },
+    },
+    plugins: [],
 };
