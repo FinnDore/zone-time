004748def2ca2809d1a53b891cf83f2b7c908daa
Finn <34718806+FinnDore@users.noreply.github.com>
2022-10-13 20:26:55 +0100

    feat: added a basic og image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,10 @@ export default function document() {
             <Head>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="icon" href="/favicon.ico" />
-
+                <meta
+                    property="og:image"
+                    content="https://time.finndore.dev/api/og"
+                />
                 <meta name="theme-color" content="#000" />
             </Head>
             <body className=" bg-[#1e2022] text-white ">
---
+++ src/pages/api/og.tsx
@@ -0,0 +1,54 @@
+import { ImageResponse } from '@vercel/og';
+import clsx from 'clsx';
+import { random } from 'lodash-es';
+import { NextApiHandler } from 'next';
+
+type FakeIntl = {
+    supportedValuesOf: (t: string) => string[];
+};
+function getRandom<T>(list: T[]): T {
+    return list[random(0, list.length - 1)] as T;
+}
+
+const zones = (Intl as unknown as FakeIntl).supportedValuesOf('timeZone');
+const handler: NextApiHandler = () => {
+    const date = new Date();
+    const timeFormat = date.toLocaleTimeString('utc', {
+        hour: 'numeric',
+        minute: 'numeric',
+        timeZoneName: 'shortGeneric',
+        timeZone: getRandom(zones),
+    });
+
+    const isMorning = date.getHours() < 12;
+
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    height: '100%',
+                    width: '100%',
+                    display: 'flex',
+                    background: '#000',
+                }}
+            >
+                <div tw="w-[100%] h-[100%] flex text-center">
+                    <div
+                        tw={clsx('m-auto items-center  text-4xl', {
+                            'text-orange-800': isMorning,
+                            'text-blue-800': !isMorning,
+                        })}
+                    >
+                        {timeFormat}
+                    </div>
+                </div>
+            </div>
+        )
+    );
+};
+
+export default handler;
+
+export const config = {
+    runtime: 'experimental-edge',
+};
