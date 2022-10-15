1640b4779c35fbe77a1b5cd6cd33af2c36276b2c
Finn <34718806+FinnDore@users.noreply.github.com>
2022-10-15 19:39:10 +0100

    feat: og improvements

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,15 +3,13 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function document() {
     return (
         <Html>
-            <title>Zone Time</title>
+            <title>Time</title>
             <Head>
-                <link rel="icon" href="/favicon.ico" />
                 <link rel="icon" href="/favicon.ico" />
                 <meta
                     property="og:image"
                     content="https://time.finndore.dev/api/og"
                 />
-                <meta name="theme-color" content="#000" />
                 <meta name="twitter:card" content="summary_large_image"></meta>
             </Head>
             <body className=" bg-[#1e2022] text-white ">
