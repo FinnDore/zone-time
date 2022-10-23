42b8ea82d45d8765b35f04f2730215fbbf21cf87
Finn <34718806+FinnDore@users.noreply.github.com>
2022-10-23 15:51:38 +0100

    added umami

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,19 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function document() {
+    const isProd = process.env.NODE_ENV === 'production';
     return (
         <Html>
             <title>Time</title>
             <Head>
+                {isProd && (
+                    <script
+                        async
+                        defer
+                        data-website-id="d64d0f3e-157e-4606-9dd9-2d3ffd1072da"
+                        src="https://umami.finndore.dev/umami.js"
+                    ></script>
+                )}
                 <link rel="icon" href="/favicon.ico" />
                 <meta
                     property="og:image"
